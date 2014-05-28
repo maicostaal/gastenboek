8ac88e43fe829fc2475d5c6734c04cbaa17eaad0
maicostaal <maicostaal@gmail.com>
2014-05-28 13:30:33 +0200

    Tabellenstructuur + verbinding database

--- gastenboek.php
+++ gastenboek.php
@@ -6,6 +6,28 @@
  * Time: 22:02
  */
 
+error_reporting(E_ALL);
+$host = "localhost"; // Je host
+$user = "maico"; // Je MySQL gebruikersnaam
+$pass = "test"; // Je MySQL wachtwoord
+$datb = "test"; // Je MySQL database
+
+// Verbinding maken
+mysql_connect($host, $user, $pass) or die ("Er is iets mis gegaan");
+mysql_select_db($datb) or die ("Er is iets mis gegaan");
+
+// UBB aanmaken
+function ubb($string) {
+    $string = htmlspecialchars($string); // Beveiligen voor XSS injection
+    $string = stripslashes($string); // Slashes verwijderen
+    $string = nl2br($string); // Zorgen dat er meerdere regels gebruikt kunnen worden
+    $string = preg_replace("#\[b\](.+?)\[/b\]#is", "<b>\\1</b>", $string); // [b][/b] => <b></b>
+    $string = preg_replace("#\[i\](.+?)\[/i\]#is", "<i>\\1</i>", $string); // [i][/i] => <i></i>
+    $string = preg_replace("#\[u\](.+?)\[/u\]#is", "<u>\\1</u>", $string); // [u][/u] => <u></u>
+    $string = preg_replace("#\[s\](.+?)\[/s\]#is", "<s>\\1</s>", $string); // [s][/s] => <s></s>
+
+    return $string;
+}
 
 ?>
 
@@ -60,6 +82,8 @@ input:focus, textarea:focus{
 
 <div style="width:500px; margin:0 auto;">
 <h1 style="text-align:center;">Gastenboek</h1>
+
+
 
 
 
